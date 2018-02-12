22a51ce2ccba3fa119a67c401acc7760661ae003
boredomdenied <rackstackandrender@gmail.com>
2018-02-11 20:07:40 -0600

    added JWT

--- app.go
+++ app.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"crypto/rsa"
 	"encoding/json"
+	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"gopkg.in/mgo.v2/bson"
-
-	"github.com/gorilla/mux"
+	"strings"
+	"time"
 	. "users-restapi/config"
 	. "users-restapi/dao"
 	. "users-restapi/models"
@@ -16,45 +22,123 @@ import (
 var config = Config{}
 var dao = UsersDAO{}
 
+const (
+
+	// For simplicity these files are in the same folder as the app binary.
+
+	privKeyPath = "app.rsa"
+	pubKeyPath  = "app.rsa.pub"
+)
+
+var (
+	verifyKey *rsa.PublicKey
+	signKey   *rsa.PrivateKey
+)
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initKeys() {
+	signBytes, err := ioutil.ReadFile(privKeyPath)
+	fatal(err)
+
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	fatal(err)
+
+	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
+	fatal(err)
+
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	fatal(err)
+}
+
+type Response struct {
+	Data string `json:"data"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
 // GET list of users
+
 func AllUsersEndPoint(w http.ResponseWriter, r *http.Request) {
+	response := ""
 	users, err := dao.FindAll()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, users)
+	respondWithJson(w, http.StatusOK, response, users)
 }
 
 // GET a user by its ID
+
 func FindUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	response := ""
 	params := mux.Vars(r)
 	user, err := dao.FindById(params["id"])
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
 		return
 	}
-	respondWithJson(w, http.StatusOK, user)
+	respondWithJson(w, http.StatusOK, response, user)
 }
 
 // POST a new user
+
 func CreateUserEndPoint(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
+
 	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, "Error in request")
 		return
 	}
+
+	if strings.ToLower(user.Username) == "" {
+		if user.Password == "" {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Println("Error logging in")
+			fmt.Fprint(w, "Invalid credentials")
+			return
+		}
+	}
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+	claims["iat"] = time.Now().Unix()
+	token.Claims = claims
+
+	tokenString, err := token.SignedString(signKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error while signing the token")
+		fatal(err)
+	}
+
+	response := Token{tokenString}
+
 	user.ID = bson.NewObjectId()
 	if err := dao.Insert(user); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusCreated, user)
+	respondWithJson(w, http.StatusCreated, response, user)
 }
 
 // PUT update an existing user
+
 func UpdateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	response := ""
 	defer r.Body.Close()
 	var user User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
@@ -65,11 +149,13 @@ func UpdateUserEndPoint(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	respondWithJson(w, http.StatusOK, response, map[string]string{"result": "success"})
 }
 
 // DELETE an existing user
+
 func DeleteUserEndPoint(w http.ResponseWriter, r *http.Request) {
+	response := ""
 	defer r.Body.Close()
 	var user User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
@@ -80,21 +166,27 @@ func DeleteUserEndPoint(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	respondWithJson(w, http.StatusOK, response, map[string]string{"result": "success"})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
+	response := ""
+	respondWithJson(w, code, response, map[string]string{"error": msg})
 }
 
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
+func respondWithJson(w http.ResponseWriter, code int, response interface{}, payload interface{}) {
+	response1, _ := json.Marshal(payload)
+	response2, _ := json.Marshal(response)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(response)
+	w.Write(response1)
+	w.Write(response2)
+
 }
 
 // Parse the configuration file 'config.toml', and establish a connection to DB
+
 func init() {
 	config.Read()
 
@@ -104,14 +196,105 @@ func init() {
 }
 
 // Define HTTP request routes
-func main() {
+
+func StartServer() {
 	r := mux.NewRouter()
-	r.HandleFunc("/users", AllUsersEndPoint).Methods("GET")
 	r.HandleFunc("/users", CreateUserEndPoint).Methods("POST")
-	r.HandleFunc("/users", UpdateUserEndPoint).Methods("PUT")
-	r.HandleFunc("/users", DeleteUserEndPoint).Methods("DELETE")
-	r.HandleFunc("/users/{id}", FindUserEndpoint).Methods("GET")
+	r.HandleFunc("/users", AllUsersEndPoint).Methods("GET")
+
+	r.Handle("/users/{id}", negroni.New(
+		negroni.HandlerFunc(ValidateTokenMiddleware),
+		negroni.Wrap(http.HandlerFunc(ProtectedHandler)),
+	))
+
 	if err := http.ListenAndServe(":3100", r); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+
+	initKeys()
+	StartServer()
+}
+
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+
+	response := Response{"Gained access to protected resource"}
+	JsonResponse(response, w)
+
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+	var user UserCredentials
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, "Error in request")
+		return
+	}
+
+	if strings.ToLower(user.Username) == "" {
+		if user.Password == "" {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Println("Error logging in")
+			fmt.Fprint(w, "Invalid credentials")
+			return
+		}
+	}
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+	claims["iat"] = time.Now().Unix()
+	token.Claims = claims
+
+	tokenString, err := token.SignedString(signKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error while signing the token")
+		fatal(err)
+	}
+
+	response := Token{tokenString}
+	JsonResponse(response, w)
+
+}
+
+func ValidateTokenMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+	token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+		func(token *jwt.Token) (interface{}, error) {
+			return verifyKey, nil
+		})
+
+	if err == nil {
+		if token.Valid {
+			next(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Token is not valid")
+		}
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Unauthorized access to this resource")
+	}
+
+}
+
+func JsonResponse(response interface{}, w http.ResponseWriter) {
+
+	json, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
--- models/user.go
+++ models/user.go
@@ -2,11 +2,17 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 // Represents a movie, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
 type User struct {
 	ID          bson.ObjectId 	`bson:"_id" json:"id"`
-	Name        string        	`bson:"name" json:"name"`
-	Email  		string        	`bson:"email" json:"email"`
+	Username        string        	`bson:"username" json:"username"`
+//	Email  		string        	`bson:"email" json:"email"`
 	Password 	string        	`bson:"password" json:"password"`
 }
+
